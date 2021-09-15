d89f679f2ef4bfa59f13a4c1482a86f15b10e2c9
Mark Borchard <mark@basewinner.com>
2021-09-14 17:21:20 -0700

    Initial Commit

---
+++ average-pair.js
@@ -0,0 +1,22 @@
+// add whatever parameters you deem necessary
+// add whatever parameters you deem necessary
+function averagePair(arr, target) {
+  // make pointers at start and end
+  let left = 0;
+  let right = arr.length - 1;
+
+  // run loop until the paths cross or the truthy answer is found
+  while (left < right) {
+    let avg = (arr[left] + arr[right]) / 2;
+    if (avg === target) {
+      return true;
+    } else if (avg < target) {
+      left++;
+    } else {
+      right--;
+    }
+  }
+  return false;
+}
+
+module.exports = { averagePair };
